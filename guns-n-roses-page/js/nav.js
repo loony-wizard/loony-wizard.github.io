521c7e14bd0e0e18e7b393de046e41aa257b42c8
Вова <>
2017-02-20 00:26:13 +0400

    small changes in nav-bar behavior (guns n roses page)

--- guns-n-roses-page/js/nav.js
+++ guns-n-roses-page/js/nav.js
@@ -2,4 +2,9 @@ $(document).ready(function() {
 	$('.menu-icon').click(function() {
 		$('.nav-wrapper').toggleClass('nav-opened');
 	});
+	$('.nav .btn').click(function() {
+		if ($('.nav-wrapper').hasClass('nav-opened')) {
+			$('.nav-wrapper').toggleClass('nav-opened');
+		}
+	});
 })
